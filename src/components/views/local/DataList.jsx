f0b3ef23be893f00aa96ee0fc0f90ea35f67c636
ipapMaster <wmast@inbox.ru>
2024-01-04 18:08:24 +0300

    Filtering of Operations

--- src/components/views/local/DataList.jsx
+++ src/components/views/local/DataList.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "../../../styles/dataList";
 
-const { DataContainer, ContentLine, ContentCell } = css
+const { DataContainer, ContentLine, ContentCell, ButtonsLine, ButtonItem } = css
+
 
 const DataList = (props) => {
 
     const { data = [] } = props
+    const [dataType, setDataType] = useState('расход')
+    const filterData = data.filter(item => item.split('::')[1] === dataType)
+
+    const reduceDataType1 = () => setDataType('доход')
+    const reduceDataType2 = () => setDataType('расход')
+    const reduceDataType3 = () => setDataType('')
+
+    // Не очень хорошая практика (код дублируется)
 
     return (
         <React.Fragment>
+            <ButtonsLine>
+                <ButtonItem onClick={reduceDataType1}>доходы</ButtonItem>
+                <ButtonItem onClick={reduceDataType2}>расходы</ButtonItem>
+                <ButtonItem onClick={reduceDataType3}>общее</ButtonItem>
+            </ButtonsLine>
             <DataContainer>
-                {data.map((item, index) => {
-                    return (
-                        <ContentLine key={index}>
-                            <ContentCell width={"20%"}>{item.split('::')[0]}</ContentCell>
-                            <ContentCell width={"20%"}>{item.split('::')[1]}</ContentCell>
-                            <ContentCell width={"60%"}>{item.split('::')[2]}</ContentCell>
-                        </ContentLine>
-                    )
-                })}
+                {filterData.length > 0 && <React.Fragment>
+                    {filterData.map((item, index) => {
+                        return (
+                            <ContentLine key={index}>
+                                <ContentCell width={"20%"}>{item.split('::')[0]}</ContentCell>
+                                <ContentCell width={"20%"}>{item.split('::')[1]}</ContentCell>
+                                <ContentCell width={"60%"}>{item.split('::')[2]}</ContentCell>
+                            </ContentLine>
+                        )
+                    })}
+                </React.Fragment>}
+                {filterData.length === 0 && <React.Fragment>
+                    {data.map((item, index) => {
+                        return (
+                            <ContentLine key={index}>
+                                <ContentCell width={"20%"}>{item.split('::')[0]}</ContentCell>
+                                <ContentCell width={"20%"}>{item.split('::')[1]}</ContentCell>
+                                <ContentCell width={"60%"}>{item.split('::')[2]}</ContentCell>
+                            </ContentLine>
+                        )
+                    })}
+                </React.Fragment>}
             </DataContainer>
         </React.Fragment>
     )
