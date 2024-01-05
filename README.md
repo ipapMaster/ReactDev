b4427af36afc4ba25935980f199ece46a5f39231
ipapMaster <wmast@inbox.ru>
2024-01-05 15:35:34 +0300

    RadioGroups Added

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ https://ru.react.js.org/
 https://babeljs.io/
 https://www.techgrapple.com/online-tools/babel-javascript-compiler-convert-to-browser-compatible-js/
 https://styled-components.com/ (https://styled-components.com/docs/basics#installation)
+https://mui.com/material-ui/getting-started/installation/ (npm install @mui/material @emotion/react @emotion/styled)
+https://mui.com/material-ui/react-radio-button/ (Male and Femail)
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
 import Footer from "../views/global/Footer";
 import InputComponent from "../comps/Input";
 import css from "../../styles/form";
@@ -7,12 +12,12 @@ const { FormContainer, Button } = css
 
 const Main = (props) => {
 
-    const {action} = props
+    const { action } = props
 
     const [value, setValue] = useState('')
-    const [type, setType] = useState('')
+    const [type, setType] = useState('доход')
     const [comment, setComment] = useState('')
-    
+
 
     const validation = () => {
         if (value.length > 2 && type) {
@@ -29,12 +34,49 @@ const Main = (props) => {
         }
     }
 
+    const handleChange = (event) => {
+        setType(event.target.value);
+    };
+
+    const handleChangeComment = (event) => {
+        setComment(event.target.value);
+    };
+
     return (
         <React.Fragment>
             <FormContainer>
                 <InputComponent inputValue={value} action={setValue} placeholder={"Введите сумму транзакции"} />
-                <InputComponent inputValue={type} action={setType} placeholder={"Введите тип транзакции"} />
-                <InputComponent inputValue={comment} action={setComment} placeholder={"Введите комментарий"} />
+                <FormControl>
+                    <FormLabel id="demo-controlled-radio-buttons-group">Выберите тип транзакции</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="demo-controlled-radio-buttons-group"
+                        name="controlled-radio-buttons-group"
+                        value={type}
+                        onChange={handleChange}
+                        style={{ marginTop: '5px', marginLeft: '6px' }}
+                    >
+                        <FormControlLabel value="расход" control={<Radio />} label="Расход" />
+                        <FormControlLabel value="доход" control={<Radio />} label="Доход" />
+                    </RadioGroup>
+                </FormControl>
+                {type === 'доход' && <InputComponent inputValue={comment} action={setComment} placeholder={"Введите комментарий"} />}
+                {type === 'расход' && <FormControl>
+                    <FormLabel id="demo-controlled-radio-buttons-group">Выберите тип расхода</FormLabel>
+                    <RadioGroup
+                        aria-labelledby="demo-controlled-radio-buttons-group"
+                        name="controlled-radio-buttons-group"
+                        value={comment}
+                        onChange={handleChangeComment}
+                        style={{ marginTop: '5px', marginLeft: '6px' }}
+                    >
+                        <FormControlLabel value="покупка продуктов" control={<Radio />} label="Покупка продуктов" />
+                        <FormControlLabel value="оплата счетов" control={<Radio />} label="Оплата счетов" />
+                        <FormControlLabel value="покупка одежды" control={<Radio />} label="Покупка одежды" />
+                        <FormControlLabel value="расходы на транспорт" control={<Radio />} label="Расходы на транспорт" />
+                        <FormControlLabel value="развлечения" control={<Radio />} label="Развлечения" />
+                        <FormControlLabel value="путешествия" control={<Radio />} label="Путешествия" />
+                    </RadioGroup>
+                </FormControl>}
                 <Button
                     backgroundColor={
                         value.length < 3 ?
