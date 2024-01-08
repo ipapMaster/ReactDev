0a5b77d97b880c779008a14270003deacb33176c
ipapMaster <wmast@inbox.ru>
2024-01-08 19:46:58 +0300

    TimeString Rendering

--- src/components/pages/Plan.jsx
+++ src/components/pages/Plan.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Plan = () => {
+
+    const [time, setTime] = useState()
+
+    useEffect(() => {
+
+        setInterval(() => setTime(new Date().toLocaleTimeString()), 1000)
+
+    }, [])
 
     return (
         <React.Fragment>
             <span style={{ display: 'block', marginTop: '100px', marginLeft: '130px' }}>страница планирования</span>
+            <span style={{ display: 'block', marginTop: '10px', marginLeft: '130px' }}>время - {time}</span>
         </React.Fragment>
     )
 }
